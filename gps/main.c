7e86fa89c7469c3c31ee08fcac7a5bb77ef23070
20161104598 <a20161104598@ciecimac17deimac.local>
2017-06-07 09:13:53 +0800

    Initial Commit

---
+++ gps/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  gps
+//
+//  Created by 20161104598 on 17/6/7.
+//  Copyright © 2017年 20161104598. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
