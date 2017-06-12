821c27a081f72b546fee5b1a0f44f9737d85b916
20161104598 <a20161104598@ciecimac17deiMac.local>
2017-06-12 11:55:13 +0800

    paixu

--- gps/main.c
+++ gps/main.c
@@ -7,9 +7,31 @@
 //
 
 #include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main()
+{
+    int n,i,j;
+    int s[20];
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        scanf("%d",&n);
+    }
+    for(j=1;j<=n-1;j++)
+    {
+        for(i=0;i<=n-1-j;i++)
+        {
+            if(s[i]>s[i+1])
+            {
+                s[19]=s[i];
+                s[i]=s[i+1];
+                s[i+1]=s[19];
+            }
+        }
+    }
+    for(i=0;i<=10;i++)
+    {
+        printf("%d",s[n]);
+    }
+    
     return 0;
 }
